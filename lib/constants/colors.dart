d397eaf174f73c3a599112c10594202d883ff547
Ankita Soni <ankitasoni018@gmail.com>
2024-01-09 17:21:43 +0530

    constants added

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const blue = Color.fromARGB(255, 32, 67, 224);
+const darkBlue = Color(0xFF3442AF);
+const bgColor = Color(0xFFE4ECFA);
+const lightGrey = Color(0xFFB8C6E6);
