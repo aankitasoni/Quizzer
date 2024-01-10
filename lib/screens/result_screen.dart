5f9bcba315a37efbba652ff3f8eda7d2c4e127c6
Ankita Soni <ankitasoni018@gmail.com>
2024-01-10 13:19:10 +0530

    indicator added

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -3,11 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:quiz_app3/constants/text_style.dart';
 import 'package:quiz_app3/screens/home_screen.dart';
+import 'package:quiz_app3/widgets/custom_button.dart';
 
 import '../constants/colors.dart';
 
 class ResultScreen extends StatefulWidget {
-  const ResultScreen({super.key});
+  final int points;
+
+  const ResultScreen({
+    super.key,
+    required this.points,
+  });
 
   @override
   State<ResultScreen> createState() => _ResultScreenState();
@@ -18,94 +24,87 @@ class _ResultScreenState extends State<ResultScreen> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Results"),
+          centerTitle: true,
+          leading: InkWell(
+            child: const Icon(
+              CupertinoIcons.xmark,
+            ),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomeScreen(),
+                ),
+              );
+            },
+          ),
+        ),
+        body: Container(
           width: double.infinity,
           height: double.infinity,
           padding: const EdgeInsets.all(12),
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [lightGrey, darkBlue],
-            ),
-          ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border.all(
-                        color: lightGrey,
-                        width: 2,
-                      ),
-                    ),
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(
-                        CupertinoIcons.xmark,
-                        color: Colors.white,
-                        size: 28,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
               Column(
                 children: [
                   headingText("Your Final Score", Colors.black, 28),
                   const SizedBox(
-                    height: 20,
+                    height: 50,
                   ),
                   CircularPercentIndicator(
                     radius: 100.0,
                     lineWidth: 10.0,
-                    percent: 0.8,
-                    center: const Icon(
-                      Icons.person_pin,
-                      size: 50.0,
-                      color: Colors.white,
-                    ),
-                    backgroundColor: lightGrey,
-                    progressColor: blue,
-                  ),
-
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const HomeScreen(),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.only(bottom: 30),
-                        alignment: Alignment.center,
-                        width: size.width - 100,
-                        padding: const EdgeInsets.all(14),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: headingText(
-                          "Try Another",
-                          blue,
-                          18,
-                        ),
+                    percent: (widget.points.toDouble()) / 100,
+                    center: Text(
+                      "${widget.points}/20",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )
+                    backgroundColor: lightGrey,
+                    progressColor: Colors.red,
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Text(
+                    "Correct Answer: ${widget.points}",
+                    style: const TextStyle(
+                      color: Colors.green,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Incorrect Answer: ${20 - widget.points}",
+                    style: const TextStyle(
+                      color: Colors.red,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  CustomButton(
+                    buttonText: "Try Another",
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomeScreen(),
+                        ),
+                      );
+                    },
+                  ),
                 ],
               ),
             ],
